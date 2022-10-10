a0afb0be24d0c8d02f378d62868d7d12c6ca0a89
Soubhagya118 <rinkisoubhagyasamal118@gmail.com>
2022-10-10 19:36:02 +0530

    updated 2ndpushed file name from 2nd location

--- JS-01/problem2.js
+++
@@ -1,6 +0,0 @@
-var name="Rinki";
-console.log(name);
-name ="S.K.Samal";
-console.log(name);
-name="Rebati";
-console.log(name);
--- JS-01/problem3.js
+++
@@ -1,4 +0,0 @@
-var name ="rinki";
-var age=21;
-console.log("name"+"-"+name+"  "+"age"+"-"+age);
-console.log(name+"-"+typeof name+"  "+age+"-"+typeof age);
